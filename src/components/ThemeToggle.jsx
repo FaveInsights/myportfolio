44472c84c2159b3e883733a32ffb229fb2279bc6
favourbadakin <favourbadakin@gmail.com>
2025-01-04 21:09:34 +0100

    somr changes

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext, useState} from "react";
 import { ThemeContext } from "../ThemeContext";
 import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 import Tippy from "@tippyjs/react";
@@ -8,12 +7,21 @@ import "tippy.js/dist/tippy.css";
 const ThemeToggle = () => {
   const { theme } = useContext(ThemeContext);
   const { toggleTheme } = useContext(ThemeContext);
+  const [isTranstioning, setIsTransitioning] = useState(false);
+  const handleThemeToggle = () =>{
+    toggleTheme();
+    setIsTransitioning(true);
+    setTimeout(()=>{
+      setIsTransitioning(false)
+    },500);
+  };
   return (
     <Tippy
-      content={theme === "dark" ? "Toggle Light Mode" : "Toggle Dark Mode"}
+      content={theme === "dark" ? "Light Mode" : "Dark Mode"}
+      animation="fade"
+      duration={600}
     >
-      <button onClick={toggleTheme}>
-       
+      <button onClick={handleThemeToggle} className={isTranstioning ? "trans" : ''}>
         {theme === "dark" ? <MdOutlineLightMode /> : <MdDarkMode />}
       </button>
     </Tippy>
