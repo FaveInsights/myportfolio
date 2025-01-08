f981e57f3ef99e872fdfa3291082150bb15a0b53
favourbadakin <favourbadakin@gmail.com>
2025-01-08 15:38:24 +0100

    some changes

--- src/pages/EdnaDashboard.jsx
+++ src/pages/EdnaDashboard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import overview from "../assets/images/overview.JPG";
-import p2018 from "../assets/images/2018.JPG";
-import p2019 from "../assets/images/2019.JPG";
-import p2020 from "../assets/images/2020.JPG";
-import p2021 from "../assets/images/2021.JPG";
-import revenue from "../assets/images/revenue.JPG";
-import sites from "../assets/images/sites.JPG";
-import managers from "../assets/images/managers.JPG";
-import calls from "../assets/images/calls.JPG";
+import overview from "../assets/images/overview.jpg";
+import p2018 from "../assets/images/2018.jpg";
+import p2019 from "../assets/images/2019.jpg";
+import p2020 from "../assets/images/2020.jpg";
+import p2021 from "../assets/images/2021.jpg";
+import revenue from "../assets/images/revenue.jpg";
+import sites from "../assets/images/sites.jpg";
+import managers from "../assets/images/managers.jpg";
+import calls from "../assets/images/calls.jpg";
 
 const EdnaDashboard = () => {
   return (
@@ -16,7 +16,7 @@ const EdnaDashboard = () => {
         <div>
           <img src={overview} alt="dashboard" />
         </div>
-        {/* <div>
+        <div>
           <img src={p2018} alt="dashboard" />
         </div>
         <div>
@@ -39,7 +39,7 @@ const EdnaDashboard = () => {
         </div>
         <div>
           <img src={revenue} alt="dashboard" />
-        </div> */}
+        </div>
       </main>
     </>
   );
