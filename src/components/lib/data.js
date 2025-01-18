3f20d97aec16768965347d77aac7309f0298d4cb
favourbadakin <favourbadakin@gmail.com>
2025-01-18 19:08:58 +0100

    some changes

--- src/components/lib/data.js
+++ src/components/lib/data.js
@@ -6,13 +6,14 @@ import call2 from "../../assets/images/call2.jpg";
 
 export default [
   {
-    
     id: 1,
     title: "EDNA Call Center Analysis",
     desc: "Comprehensive report that provides valuable insights, recommends productivity enhancements, and revenue-boosting strategies for the company's growth.",
     image: call2,
     tool: "PowerBI, Excel",
-    medium: "https://medium.com/favourbadakin",
+    medium:
+      "https://medium.com/@favourbadakin/call-center-performance-analysis-identifying-opportunities-for-improvement-5536756772b3",
+    github: "https://www.github.com/FaveInsights/EDNA-Call-Center-Analysis",
   },
   {
     id: 2,
@@ -20,6 +21,8 @@ export default [
     desc: "Analyzing customer data to identify churn drivers and inform targeted retention strategies for improved customer satisfaction.",
     image: telecom,
     tool: "PowerBI, Excel",
+    github:
+      "https://www.github.com/FaveInsights/Telecommunication-Company-Churn-Analysis",
   },
   {
     id: 3,
@@ -27,13 +30,14 @@ export default [
     desc: "Uncovering hidden insights from a raw data to aid in making strategic decisions that promote efficiency and drive growth.",
     image: call1,
     tool: "PowerBI",
+    github: "https://www.github.com/FaveInsights/Call-Center-Analysis",
   },
   {
     id: 4,
     title: "Skilled Migration Analysis",
     desc: "Extraction of different categories of data from a large database and performing analysis on them.",
     image: migration,
-    github: "",
+    github: "https://www.github.com/FaveInsights/Skilled-Migration-Analysis",
     tool: "SQL",
   },
   {
@@ -42,5 +46,6 @@ export default [
     desc: "Cleaning a dirty and messy dataset of certain club members and preparing it to ensure an accurate and reliable analysis.",
     image: club,
     tool: "SQL",
+    github:'https://www.github.com/FaveInsights/Club-Member-SQL-Data-Cleaning'
   },
 ];
