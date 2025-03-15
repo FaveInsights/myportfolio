d1e1ccdd0b48f4ed295164912ab56e0d82f7cc66
Faour Badakin <oludarebadakin@gmail.com>
2025-03-15 10:55:30 +0100

    new

--- src/pages/Smedia.jsx
+++ src/pages/Smedia.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import DashboardNav from '../components/DashboardNav'
 import social from '../assets/images/socialmedia2.jpg'
-import social1 from '../assets/images/socialmediaa.JPG'
+import social1 from '../assets/images/socialmediaa.jpg'
 
 const Smedia = () => {
   return (
